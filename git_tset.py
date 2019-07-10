df520a638d1adf6457b008c64c1a45a61a5df7bb
610 <victor074and610@yahoo.com.tw>
2019-07-10 09:43:00 +0800

    測試工作中 遇問題回朔

--- git_tset.py
+++ git_tset.py
@@ -3,5 +3,9 @@ print(gogogo  nomber one)
 print(gogogo  nomber one6666)
 
 
+<<<<<<< Updated upstream
 print(gogogo  nomber one6666---2)
 
+=======
+print(gogogo  nomber one6666 stash)
+>>>>>>> Stashed changes
