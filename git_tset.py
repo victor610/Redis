a93fadf7b4662062ca526fadce0b5e4fae2a1a56
610 <victor074and610@yahoo.com.tw>
2019-07-10 11:36:32 +0800

    第二次測試DEV修改

--- git_tset.py
+++ git_tset.py
@@ -4,9 +4,8 @@ print(gogogo  nomber one6666)
 
 print(454564654564)
 
-print(gogogo  nomber one6666---8888)
-git stash
+print(gogogo  nomber one6777777---8888)
 
-print(gogogo  nomber one6666---dev 分隻  )
+print(gogogo  nomber 7777---dev 分隻  )
 
-print(gogogo  nomber one6666---dev 分隻  功能完成)
+print(gogogo  nomber one6666---dev 分隻  功能)
