7956102fdd7d05b8fe637007b4c0ded6ff580281
610 <victor074and610@yahoo.com.tw>
2019-07-10 15:49:41 +0800

    第一次公司提交不完全功能 DEV版

--- git_tset.py
+++ git_tset.py
@@ -14,5 +14,5 @@ print(gogogo  nomber one6777777---8888)
 print(gogogo  nomber 7777---dev 分隻  )
 
 
-print(gogogo  nomber one6666---dev 分隻  功能 衝突保留)
+print(gogogo  nomber one6666---dev 分隻  功能 衝突保留  dev 的公司離去)
 
