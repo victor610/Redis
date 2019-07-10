7f829bc85b3257f68b3e5ecd7341183d900716e6
610 <victor074and610@yahoo.com.tw>
2019-07-10 10:09:33 +0800

    三之 衝突測試

--- git_tset.py
+++ git_tset.py
@@ -2,10 +2,6 @@ print(gogogo  nomber one)
 
 print(gogogo  nomber one6666)
 
+print(454564654564)
 
-<<<<<<< Updated upstream
-print(gogogo  nomber one6666---2)
-
-=======
-print(gogogo  nomber one6666 stash)
->>>>>>> Stashed changes
+print(gogogo  nomber one6666---8888)
